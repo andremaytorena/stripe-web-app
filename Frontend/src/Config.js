e9c1ec07d0ad4e7f0dfa44cfe42aafaa3ccbafee
Andre Maytorena <andremayto@gmail.com>
2024-06-01 21:39:10 +0100

    s

--- Frontend/src/Config.js
+++ Frontend/src/Config.js
@@ -1,7 +1,7 @@
 
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // const API_BASE_URL = "https://api.matche.app";
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://stripe-web-app.onrender.com";
 
 export default API_BASE_URL;
 
